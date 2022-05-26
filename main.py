f0a353e8353790753dfc4a8bda2800e196e21b61
perez <pabloperezcordero@gmail.com>
2022-05-26 19:17:05 +0200

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -238,8 +238,5 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
